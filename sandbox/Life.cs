469ea8f66b6d7c8cce3d5bc223f5b9c0d226b9fe
root <root@rails.(none)>
2011-10-17 21:16:21 +0000

    11

--- sandbox/Life.cs
+++ sandbox/Life.cs
@@ -4,6 +4,11 @@ public class Life
 {
     public static string[] NextGeneration(string[] inputField)
     {
+        if(inputField == null)
+        {
+            return null;
+        }
+
         var result = new String[] {
             "........",
             "........",
