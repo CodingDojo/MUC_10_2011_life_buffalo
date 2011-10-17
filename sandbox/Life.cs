73671c43608ffbabb41d96e19922894bc8f2dd86
root <root@rails.(none)>
2011-10-17 21:39:20 +0000

    25

--- sandbox/Life.cs
+++ sandbox/Life.cs
@@ -52,7 +52,7 @@ public class Life
 
         for (int i = -1; i <=1; i++)
         {
-            for (j= -1; j<= 1; j++)
+            for (int j= -1; j<= 1; j++)
             {
                 if (i == 0 || j==0)
                 {
